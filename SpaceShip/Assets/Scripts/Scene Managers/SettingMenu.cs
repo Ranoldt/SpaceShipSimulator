88ba111b37937bc474a8c28510280e5b126800e3
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2023-02-08 23:07:12 -0800

    pause/settings

--- SpaceShip/Assets/Scripts/Scene Managers/SettingMenu.cs
+++ SpaceShip/Assets/Scripts/Scene Managers/SettingMenu.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public  class SettingMenu : MonoBehaviour
 {
     [SerializeField]
     private GameObject settingMenu;
+
+    public TMP_Dropdown resolutionDropdown;
+    
+    Resolution[] resolutions;
 
     public void openMenu()
     {
@@ -17,4 +23,41 @@ public  class SettingMenu : MonoBehaviour
         settingMenu.SetActive(false);
     }
 
+    
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionsIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionsIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionsIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullScreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 }
