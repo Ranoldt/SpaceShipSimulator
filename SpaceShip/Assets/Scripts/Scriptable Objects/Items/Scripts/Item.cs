92c34ab047302295884dc89c94b90ed2f8547490
JustYunsarts <ysroh@uci.edu>
2023-02-08 22:42:51 -0800

    Spawner created
    made it a scriptable object so we can make different kinds of spawners down the line

--- SpaceShip/Assets/Scripts/Scriptable Objects/Items/Scripts/Item.cs
+++ SpaceShip/Assets/Scripts/Scriptable Objects/Items/Scripts/Item.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    //make items destroy themselves if you dont collect them in time
+    [SerializeField]
+    private float lifespan;
+
+    private float timeAlive;
+
+    private void Awake()
+    {
+        timeAlive = 0f;
+    }
+    private void Update()
+    {
+        if (timeAlive >= lifespan)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        timeAlive += Time.deltaTime;
+    }
+
+
     public ItemObject item;
 }
