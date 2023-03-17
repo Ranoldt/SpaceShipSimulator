e8d6555d1a301474827fb467d02f47b407641146
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2023-03-17 14:38:39 -0700

    playerAI (inprogress2)

--- SpaceShip/Assets/Scripts/PlayerAI.cs
+++ SpaceShip/Assets/Scripts/PlayerAI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerAI : MonoBehaviour
 {
     [SerializeField]
+    private playerAI AIplayer;
+    [SerializeField]
     Transform[] targets;
     [SerializeField]
     public float movementSpeed = 10f;
@@ -12,25 +14,84 @@ public class PlayerAI : MonoBehaviour
     public float rotationalDamp = 1f;
     [SerializeField]
     private Transform Middle;
+    [SerializeField]
+    int Range = 10;
+    int height = 1;
+    [SerializeField]
+    private LevelUp levels;
+    public List<LineRenderer> beams = new List<LineRenderer>();
+    private MineObjects tool { get { return GetComponent<SpaceShip>().inv.equippedMineTool; } }
+    public InventoryManager inv;
 
-    private float LaserRange = 100f;
+
+    Vector3 wayPoint;
+
+    private float SightRange = 30;
+    private float health;
     RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Wander();
+        health = 100f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(AIplayer)
+        {
+            case playerAI.wander:
+                Chase(wayPoint);
+                if ((transform.position - wayPoint).magnitude < 3)
+                    Wander();
+                if (HaveLineOfSight("Collects"))
+                    AIplayer = playerAI.collect;
+                if (HaveLineOfSight("Asteroids"))
+                    AIplayer = playerAI.mine;
+                break;
+            case playerAI.collect:
+                GameObject[] CollectTargets = GameObject.FindGameObjectsWithTag("Collects");
+                for (int i = 0; i < CollectTargets.Length; i++)
+                {
+                    if (HaveLineOfSight("Collects") && InFront("Collects"))
+                    {
+                        Chase(CollectTargets[i].transform.position);
+                        break;
+                    }
+                    else
+                        AIplayer = playerAI.wander;
+                }
+                break;
+            case playerAI.mine:
+                if (HaveLineOfSight("Asteroids") && InFront("Asteroids"))
+                    Mine();
+                break;
+            case playerAI.attack:
+                break;
+            case playerAI.sell:
+                break;
+            case playerAI.die:
+                Destroy(this.gameObject);
+                break;
+        }
     }
 
-    bool InFront(int targetIndex)
+    public void damage(float damage)
     {
-        Transform target = targets[targetIndex];
-            Vector3 directionToTarget = transform.position - target.position;
+        health -= damage;
+        if (health <= 0)
+        {
+             AIplayer= playerAI.die;
+        }
+    }
+
+    bool InFront(string tag)
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject target in targets)
+        {
+            Vector3 directionToTarget = transform.position - target.transform.position;
             float angle = Vector3.Angle(transform.forward, directionToTarget);
 
 
@@ -38,28 +99,31 @@ public class PlayerAI : MonoBehaviour
             {
                 return true;
             }
+        }
         return false;
     }
 
-    bool HaveLineOfSight(string tag, int targetIndex)
+    bool HaveLineOfSight(string tag)
     {
-        Transform target = targets[targetIndex];
-        Vector3 direction = target.position - transform.position;
-
-        if (Physics.Raycast(Middle.transform.position, direction, out hit, LaserRange))
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(tag);
+        foreach(GameObject target in targets)
         {
-            if (hit.transform.CompareTag(tag))
+            Vector3 direction = target.transform.position - transform.position;
+
+            if (Physics.Raycast(Middle.transform.position, direction, out hit, SightRange))
             {
-                return true;
+                if (hit.transform.CompareTag(tag))
+                {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    void Chase(int targetIndex)
+    void Chase(Vector3 destination)
     {
-        Transform target = targets[targetIndex];
-            Vector3 pos = target.position - transform.position;
+            Vector3 pos = destination - transform.position;
             Quaternion rotation = Quaternion.LookRotation(pos);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime);
             transform.position += transform.forward * movementSpeed * Time.deltaTime;
@@ -67,15 +131,43 @@ public class PlayerAI : MonoBehaviour
 
     void Wander()
     {
-
+        wayPoint = new Vector3(Random.Range(transform.position.x - Range, transform.position.x + Range), Random.Range(transform.position.y - height, transform.position.y + height), Random.Range(transform.position.z - Range, transform.position.z + Range));
+        transform.LookAt(wayPoint);
     }
-    public enum playerAI
+
+    void Mine()
+    {
+        RaycastHit Hitinfo;
+
+        if (TargetInfo.IsTargetInRange(Middle.position, Middle.forward, out Hitinfo, tool.laserRange, tool.shootingMask))
+        {
+            Debug.Log("BRUH");
+            IShootable target = Hitinfo.transform.GetComponent<IShootable>();
+            if (target != null)
+            {
+                target.damage(tool.miningPower + (1.5f * levels.minePowerLevel));//total power of laser
+            }
+            Instantiate(tool.laserHitParticles, Hitinfo.point, Quaternion.LookRotation(Hitinfo.normal));
+
+            foreach (var beam in beams)
+            {
+                Debug.Log("Beam work");
+                Vector3 localHitPosition = beam.transform.InverseTransformPoint(Hitinfo.point);
+                beam.gameObject.SetActive(true);
+                beam.SetPosition(1, localHitPosition);
+            }
+        }
+    }
+}
+public enum playerAI
     {
         wander,
         attack,
         collect,
-        mine
+        mine,
+        sell,
+        die
 
     }
   
-}
+
