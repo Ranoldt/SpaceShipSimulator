8f69e1680182682f2fd0169d1869e4ffda44188c
B-Castaneda <becasta1@uci.edu>
2023-03-17 16:22:57 -0700

    First Version of Object Pooling
    Added object pooling for "Mid Asteroids."  Might need some testing, I've noticed the asteroid either does not get destroyed or it spawns back in too quickly.

--- SpaceShip/Assets/Scripts/Spawner/Scripts/SpawnerBehavior.cs
+++ SpaceShip/Assets/Scripts/Spawner/Scripts/SpawnerBehavior.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class SpawnerBehavior : MonoBehaviour
 {
 
     [SerializeField]
     private AsteroidSpawner spawner;
+    [SerializeField]
+    private Fracture ast;
+    public ObjectPool<Fracture> asteroidPool;
 
+
+    private void Start()
+    {
+        asteroidPool = new ObjectPool<Fracture>(() =>
+        {
+            return Instantiate(ast);
+        }, ast =>
+        {
+            ast.gameObject.SetActive(true);
+        }, ast =>
+        {
+            ast.gameObject.SetActive(false);
+        }, ast =>
+        {
+            Destroy(ast.gameObject);
+        }, true, 10, 20);
+    }
+    
     private void Update()
     {
         if(this.transform.childCount < spawner.maxEntities)
@@ -18,8 +40,12 @@ public class SpawnerBehavior : MonoBehaviour
             {
                 Vector3 coords = spawner.GetCoords();
 
-                var child = Instantiate(obj, coords, Quaternion.identity);
+                
+                var child = asteroidPool.Get();
+                child.transform.position = coords;
                 child.transform.parent = gameObject.transform;
+                //var child = Instantiate(obj, coords, Quaternion.identity);
+                //child.transform.parent = gameObject.transform;
             }
         }
     }
