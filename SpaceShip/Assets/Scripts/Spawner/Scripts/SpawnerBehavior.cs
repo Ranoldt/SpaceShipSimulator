1fb5db52023b04ff467097ccee7195f741544ebb
JustYunsarts <ysroh@uci.edu>
2023-03-19 12:37:31 -0700

    idk

--- SpaceShip/Assets/Scripts/Spawner/Scripts/SpawnerBehavior.cs
+++ SpaceShip/Assets/Scripts/Spawner/Scripts/SpawnerBehavior.cs
@@ -1,35 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class SpawnerBehavior : MonoBehaviour
 {
 
     [SerializeField]
     private AsteroidSpawner spawner;
-    [SerializeField]
-    private Fracture ast;
-    public ObjectPool<Fracture> asteroidPool;
 
-
-    private void Start()
-    {
-        asteroidPool = new ObjectPool<Fracture>(() =>
-        {
-            return Instantiate(ast);
-        }, ast =>
-        {
-            ast.gameObject.SetActive(true);
-        }, ast =>
-        {
-            ast.gameObject.SetActive(false);
-        }, ast =>
-        {
-            Destroy(ast.gameObject);
-        }, true, 10, 20);
-    }
-    
     private void Update()
     {
         if(this.transform.childCount < spawner.maxEntities)
@@ -40,12 +18,8 @@ public class SpawnerBehavior : MonoBehaviour
             {
                 Vector3 coords = spawner.GetCoords();
 
-                
-                var child = asteroidPool.Get();
-                child.transform.position = coords;
+                var child = Instantiate(obj, coords, Quaternion.identity);
                 child.transform.parent = gameObject.transform;
-                //var child = Instantiate(obj, coords, Quaternion.identity);
-                //child.transform.parent = gameObject.transform;
             }
         }
     }
